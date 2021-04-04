8ffa96117b613fc09b7080ded6a06a138cb8fe81
Nick <nick@mackenzie.tech>
2021-04-04 13:06:46 -0400

    gatsby scafold

---
+++ nickmackenzie.dev-2021/src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export default function Home() {
+  return (
+  <div>
+    <nav>
+    <div className="logo">NM</div>
+    <div class="list-wrapper">      <ol>
+        <li>Home</li>
+        <li>About</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ol></div>
+
+    </nav>
+  </div>
+  )
+}
